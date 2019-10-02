71375fd354fd8e723255c43fd448f405367490a1
micaelbarreto <micaelplayer98@gmail.com>
2019-10-02 08:56:33 -0300

    final

--- src/App.js
+++ src/App.js
@@ -33,6 +33,7 @@ class App extends Component {
 
     return (
       <div className='App'>
+        <h1> Monsters </h1>
         <SearchBox 
           placeholder='search'
           handleChange={this.handleChange}
